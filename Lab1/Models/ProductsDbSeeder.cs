550fae574273fe8b9fb85f63c6765b0153261304
mgci0098 <mgelu2003@yahoo.com>
2019-05-04 11:50:56 +0300

    Adaugare primul proiect

---
+++ Lab1/Models/ProductsDbSeeder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1.Models
+{
+    public class ProductsDbSeeder
+    {
+        public static void Initialize(ProductsDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any products.
+            if (context.Products.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Products.AddRange(
+                new Product
+                {
+                    Name = "Apa",
+                    Description = "Plata",
+                    Category = "Sticla",
+                    Price = 10
+                },
+                new Product
+                {
+                    Name = "Laptop",
+                    Description = "Asus",
+                    Category = "Electronice",
+                    Price = 500
+                }
+            );
+            context.SaveChanges();
+        }
+
+
+
+    }
+}
